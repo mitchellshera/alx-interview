a4c21fa014fde0b76125c3026f147505b573e136
mitchellshera <mitchellwachera@gmail.com>
2023-12-06 16:34:25 +0300

    0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,7 +7,8 @@ def canUnlockAll(boxes):
     opened_boxes = set()
     opened_boxes.add(0)
 
-    for box_index, keys in enumerate(boxes[1:], start=1):
-        opened_boxes.update(keys)
+    for box_index, keys in enumerate(boxes):
+        if box_index in opened_boxes:
+            opened_boxes.update(keys)
 
     return len(opened_boxes) == len(boxes)
