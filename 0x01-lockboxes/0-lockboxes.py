779f1b15dbf0f382dfe8993cd8bc947aed14ab3d
mitchellshera <mitchellwachera@gmail.com>
2023-12-11 12:28:40 +0300

    trial

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,11 +7,19 @@ def canUnlockAll(boxes):
     opened_boxes = set()
     opened_boxes.add(0)
 
-    for key in opened_boxes.copy():
-        # Iterate through the keys in the currently opened boxes
-        for box_index in boxes[key]:
-            # Update the set of opened boxes
-            # with the keys inside the current box
-            opened_boxes.add(box_index)
+    # Keep track of newly discovered keys
+    new_keys = set(boxes[0])
+
+    while new_keys:
+        # Update the set of opened boxes with the newly discovered keys
+        opened_boxes.update(new_keys)
+
+        # Find new keys in the newly opened boxes
+        new_keys = set()
+        for key in opened_boxes:
+            new_keys.update(boxes[key])
+
+        # Exclude already opened boxes from the set of new keys
+        new_keys -= opened_boxes
 
     return len(opened_boxes) == len(boxes)
