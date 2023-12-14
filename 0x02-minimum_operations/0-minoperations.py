7b3df034d04982322a6705215e32edb43d04086a
mitchellshera <mitchellwachera@gmail.com>
2023-12-14 13:36:21 +0300

    0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''minoperations module'''
+
+
+def minOperations(n):
+    """minoperations to get n amount of H """
+
+
+    if n == 1:
+        return 0
+    
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+    
+    for i in range(2, n + 1):
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+    
+    return dp[n] if dp[n] != float('inf') else 0
