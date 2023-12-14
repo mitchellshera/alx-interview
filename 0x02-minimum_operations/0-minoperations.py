20d2fcc261a249676f5ce5a10766d1e6447a0962
mitchellshera <mitchellwachera@gmail.com>
2023-12-14 13:40:03 +0300

    0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,15 +6,11 @@ def minOperations(n):
     """minoperations to get n amount of H """
 
 
-    if n == 1:
-        return 0
-    
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-    
-    for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-    
-    return dp[n] if dp[n] != float('inf') else 0
+    operations = 0
+    min_operations = 2
+    while n > 1:
+        while n % min_operations == 0:
+            operations += min_operations
+            n /= min_operations
+        min_operations += 1
+    return operations
