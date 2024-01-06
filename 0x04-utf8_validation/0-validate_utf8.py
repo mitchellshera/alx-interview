d6192aca8802b18a7a733a3e11829d793ba919df
mitchellshera <mitchellwachera@gmail.com>
2024-01-06 20:53:17 +0300

    Fix indentation issue in validUTF8 function

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Determines if a given data set represents a valid UTF-8 encoding"""
 
+
 def validUTF8(data):
     # Number of bytes in the current character
     num_bytes = 0
