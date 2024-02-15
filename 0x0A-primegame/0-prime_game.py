1a932fb51cbdf0d7fc20913e6be152dc424faf09
mitchellshera <mitchellwachera@gmail.com>
2024-02-15 21:07:27 +0300

    00

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,66 +8,59 @@ the winner of the game. If Maria wins, return "Maria";
 if Ben wins, return "Ben"; if the game is a draw, return None.'''
 
 def isWinner(x, nums):
-    '''Returns the winner of the game 
-    based on the list of integers'''
+    '''Returns the winner of the game.'''
+    if x < 1 or not nums:
+        return None
+
     def sieve_of_eratosthenes(limit):
-        primes = []
-        is_prime = [True] * (limit + 1)
-        is_prime[0] = is_prime[1] = False
+        primes = [True] * (limit + 1)
+        primes[0] = primes[1] = False
 
         for num in range(2, int(limit**0.5) + 1):
-            if is_prime[num]:
-                primes.append(num)
+            if primes[num]:
                 for multiple in range(num*num, limit+1, num):
-                    is_prime[multiple] = False
-
-        for num in range(int(limit**0.5) + 1, limit + 1):
-            if is_prime[num]:
-                primes.append(num)
+                    primes[multiple] = False
 
         return primes
 
-    def is_prime(num):
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
     def optimal_move(nums):
         for num in nums:
-            if is_prime(num):
+            if primes[num]:
                 return num
         return None
 
-    maria_wins = 0
-    ben_wins = 0
+    most_wins = 0
+    winner = None
 
     for n in nums:
+        primes = sieve_of_eratosthenes(n)
         numbers = list(range(1, n + 1))
         current_player = "Maria"
+        maria_wins = 0
+        ben_wins = 0
 
         while numbers:
             prime_to_remove = optimal_move(numbers)
             if prime_to_remove is None:
-                break  # No prime number left to choose, the other player wins
+                break
+
             multiples_to_remove = [num for num in numbers if num % prime_to_remove == 0]
             numbers = [num for num in numbers if num not in multiples_to_remove]
 
-            # Switch players
+            if current_player == "Maria":
+                maria_wins += 1
+            else:
+                ben_wins += 1
+
             current_player = "Ben" if current_player == "Maria" else "Maria"
 
-        # Determine the winner based on the last move
-        if current_player == "Maria":
-            maria_wins += 1
-        else:
-            ben_wins += 1
+        round_winner = "Maria" if maria_wins > ben_wins else "Ben"
+        if round_winner == "Maria":
+            most_wins += 1
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
-    else:
+    if most_wins == 0:
         return None
+    return "Maria" if most_wins > x // 2 else "Ben"
 
+# Example usage:
+# print("Winner:", isWinner(5, [2, 5, 1, 4, 3]))
